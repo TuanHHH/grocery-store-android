7d57dfa5454b134dfec891398e3f0f5a1631075d
HuyLearnProgram <huylearnunity2021@gmail.com>
2025-05-17 17:30:59 +0700

    Giao tiep giua cac fragment

--- app/src/main/java/com/hp/grocerystore/view/activity/MainActivity.java
+++ app/src/main/java/com/hp/grocerystore/view/activity/MainActivity.java
@@ -7,22 +7,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
 import com.hp.grocerystore.R;
+import com.hp.grocerystore.model.product.Product;
+import com.hp.grocerystore.utils.FilterData;
 import com.hp.grocerystore.view.adapter.ViewpageAdapter;
+import com.hp.grocerystore.viewmodel.SharedViewModel;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 mViewPager;
     BottomNavigationView mBottomNavigationView;
     Toolbar toolbar;
+    private ImageButton btnSearch, btnClose;
+    private EditText searchBar;
+    private SharedViewModel sharedViewModel;
+    private long selectedCategoryId = -1;
+
+    private String selectedCategorySlug = ""; // Không lọc
+    private String selectedSort = "";    // Không sắp xếp
+    private int minPrice = 0;
+    private int maxPrice = 500000;
+    private String searchText = "";
 
 
     @Override
@@ -32,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
+        searchBar = findViewById(R.id.search_bar);
+        btnSearch = findViewById(R.id.btn_search);
+        btnClose = findViewById(R.id.btn_close);
+
         setSupportActionBar(toolbar);
         mViewPager = findViewById(R.id.view_pager);
         mBottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -58,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         mBottomNavigationView.setOnNavigationItemSelectedListener(new  BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {  switch (item.getItemId()){
@@ -73,12 +100,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        settingSearchFunc();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Intent intent = getIntent();
+        if (intent.hasExtra("selected_categoryId")){
+            selectedCategoryId = (long) intent.getSerializableExtra("selected_categoryId");
+        }
+        if (intent.hasExtra("selected_categorySlug")){
+            selectedCategorySlug = (String) intent.getSerializableExtra("selected_categorySlug");
+        }
+        if (intent.hasExtra("selected_sort")){
+            selectedSort = (String) intent.getSerializableExtra("selected_sort");
+        }
+        if (intent.hasExtra("min_price")){
+            minPrice = (int) intent.getSerializableExtra("min_price");
+        }
+        if (intent.hasExtra("max_price")){
+            maxPrice = (int) intent.getSerializableExtra("max_price");
+        }
+        if (intent.hasExtra("search_text")){
+            searchText = (String) intent.getSerializableExtra("search_text");
+        }
+
+        if((intent.hasExtra("selected_categorySlug") && intent.hasExtra("selected_categoryId"))
+                || intent.hasExtra("selected_sort")
+                || intent.hasExtra("min_price")
+                || intent.hasExtra("max_price")
+                || intent.hasExtra("search_text")) {
+            goToSearchFromFilter(selectedCategoryId, selectedCategorySlug,
+                    selectedSort, minPrice, maxPrice, searchText);
+        }
+    }
+
+    public void goToSearchWithCategory(long categoryId,String categorySlug) {
+        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+        FilterData data = new FilterData(categorySlug);
+        data.setSelectedCategoryId(categoryId);
+        sharedViewModel.setFilterData(data);
+        mViewPager.setCurrentItem(1, true);
+    }
+    public void goToSearchWithKeyword(String keyword) {
+        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+        FilterData data = new FilterData(keyword);
+        sharedViewModel.setFilterData(data);
+        mViewPager.setCurrentItem(1, true);
+    }
+    public void goToSearchFromFilter(long selectedCategoryId, String selectedCategorySlug, String selectedSort,
+                                     int minPrice, int maxPrice, String searchText){
+
+        sharedViewModel = new ViewModelProvider(this).get(SharedViewModel.class);
+        FilterData data = new FilterData(selectedCategoryId, selectedCategorySlug,
+                selectedSort, minPrice, maxPrice, searchText);
+        sharedViewModel.setFilterData(data);
+        mViewPager.setCurrentItem(1, true);
+    }
+    private void settingSearchFunc(){
+        // Khi nhấn Enter trên bàn phím
+        searchBar.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                    (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)) {
+                performSearch(searchBar.getText().toString().trim());
+                return true;
+            }
+            return false;
         });
 
+        // Khi nhấn icon kính lúp (đã tách riêng)
+        btnSearch.setOnClickListener(v -> {
+            performSearch(searchBar.getText().toString().trim());
+        });
+
+        // Khi nhấn nút close
+        btnClose.setOnClickListener(v -> {
+            searchBar.setText("");
+        });
+    }
+    private void performSearch(String keyword) {
+        if (keyword.contains("productName~")) goToSearchWithKeyword(keyword);
+        else goToSearchWithKeyword("productName~'"+keyword+"'");
     }
 }
